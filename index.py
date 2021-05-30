308bc2babe388e308cf1a714df078233d62160a9
Seiu <aeyis047@gmail.com>
2021-05-30 16:13:05 -0400

    init

---
+++ index.py
@@ -0,0 +1,48 @@
+import six
+from google.cloud import translate_v2 as translate
+
+samplerawtext = '손자의 전쟁 예술'
+samplecorrecttranslate = 'The art of war by Sun Tzu'
+
+def translate_text(target, text):
+    """Translates text into the target language.
+
+    Target must be an ISO 639-1 language code.
+    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+    """
+
+    translate_client = translate.Client()
+
+    if isinstance(text, six.binary_type):
+        text = text.decode("utf-8")
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.translate(text, target_language=target)
+
+    print(u"Text: {}".format(result["input"]))
+    print(u"Translation: {}".format(result["translatedText"]))
+    print(u"Detected source language: {}".format(result["detectedSourceLanguage"]))
+
+def translate_text_with_model(target, text, model="nmt"):
+    """Translates text into the target language.
+
+    Make sure your project is allowlisted.
+
+    Target must be an ISO 639-1 language code.
+    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+    """
+    translate_client = translate.Client()
+
+    if isinstance(text, six.binary_type):
+        text = text.decode("utf-8")
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.translate(text, target_language=target, model=model)
+
+    print(u"Text: {}".format(result["input"]))
+    print(u"Translation: {}".format(result["translatedText"]))
+    print(u"Detected source language: {}".format(result["detectedSourceLanguage"]))
+
+translate_text('en', samplerawtext)
